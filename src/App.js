8fdeddc74fbf1551d2dca779f5d954329158c002
Tenrah <frd.frenaud@gmail.com>
2021-02-16 15:09:20 +0100

    ça marche

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Main from "./component/Main";
+import MainBis from "./component/MainBis";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <MainBis />
     </div>
   );
 }
